132d4a97438c8a140fb8db6c40f73d6cf335ef49
Ocean0330 <107545048+Ocean0330@users.noreply.github.com>
2025-07-10 22:05:49 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <template>
-  <div></div>
+<div>
+  <div class="common-layout">
+    <el-container>
+      <el-header>
+        <router-link to="/">首页</router-link>
+        <router-link to="/about">登录/注册</router-link>
+        <router-link to="/login">证书查询</router-link>
+      </el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+    <router-view></router-view>
+  </div>
+</div>
+  
 </template>
 
 
-<script setup >
+<script lang='ts' setup >
+
+
 </script>
 
 
